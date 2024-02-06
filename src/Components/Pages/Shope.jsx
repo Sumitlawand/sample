f1bf218258522a126932804c7e311d9c4b3a996c
sanjiwanigawande <42935920+sanjiwanigawande@users.noreply.github.com>
2024-02-06 18:52:37 +0530

    first commit

---
+++ src/Components/Pages/Shope.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../Hero/Hero'
+import Popular from '../Popular/Popular'
+import Offers from '../Offers/Offers'
+import Newcollection from '../NewCollection/Newcollection'
+function Shope() {
+  return (
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      <Newcollection/>
+    </div>
+  )
+}
+
+export default Shope
